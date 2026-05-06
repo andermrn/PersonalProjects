c83cb419c9d83f9ea85bd5cced7fc322bedf5de0
Anderson Moreno <andermrn2004@gmail.com>
2026-05-06 14:26:15 -0300

    teste

--- calculadora.cpp
+++ calculadora.cpp
@@ -8,6 +8,7 @@ int main()
     // c(contador) = e o contador para o vetor
     // r(resultado) é o resultado das operaçoes
     // h(historico) é o vetor
+    teste
     int opcao, n1, n2, c = 0;
     float r, h[100];
     do
